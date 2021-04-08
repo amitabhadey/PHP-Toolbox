b774d2cc93c0c83165a2de645fd2b3763bb75dad
amitabhadey <amitabhadey.bracu@gmail.com>
2021-04-08 13:32:43 +0600

    Delete database.sql

--- database.sql
+++
@@ -1,8 +0,0 @@
-create table tooldatabase (
-    id int(11) not null PRIMARY KEY AUTO_INCREMENT,
-    toolname varchar(128) not null,
-    tooldescr varchar(128) not null,
-    toolurl varchar(2083) not null,
-    toolcategory varchar(128) not null,
-    date datetime not null
-    );
