2eb24e8a4afcd898a4a82a6c0ef2121377685688
amitabhadey <amitabhadey.bracu@gmail.com>
2021-04-08 13:50:37 +0600

    Create connecting_database.php

---
+++ connecting_database.php
@@ -0,0 +1,18 @@
+<?php
+include_once 'includes/dbh.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    $conn;
+
+    <p>Hello</p>
+
+  </body>
+</html>
